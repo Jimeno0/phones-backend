836b7d4fdcbd79f9e35ee865aeb05c421484024d
Jimeno0 <c.p.jimeno0@gmail.com>
2020-04-22 12:46:08 +0200

    add express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello world!')
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.get('/phones', (req, res) => {
+  res.json('ok')
+})
+
+app.listen(3000, () => {
+  console.log('app listening in port 3000')
+})
